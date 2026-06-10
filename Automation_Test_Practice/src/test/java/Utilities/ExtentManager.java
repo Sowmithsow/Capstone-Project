f5ace0fedbbeca7645e6cc5c4d9f3cf1c1b012cb
Sowmithran S <sowmithran@gmail.com>
2026-06-10 22:11:16 +0530

    commit

---
+++ Automation_Test_Practice/src/test/java/Utilities/ExtentManager.java
@@ -0,0 +1,33 @@
+package Utilities;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+
+        if (extent == null) {
+
+            ExtentSparkReporter spark =
+                    new ExtentSparkReporter(
+                            "Reports/ExtentReport.html");
+
+            extent = new ExtentReports();
+
+            extent.attachReporter(spark);
+
+            extent.setSystemInfo(
+                    "Tester",
+                    "Sowmithran");
+
+            extent.setSystemInfo(
+                    "Project",
+                    "Automation Test Practice");
+        }
+
+        return extent;
+    }
+}
